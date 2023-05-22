a4a294eacfd8c440ce294cbe7454cf064372170c
CORNELIUS <126483961+Cornelius-BobCat@users.noreply.github.com>
2023-05-22 15:20:14 +0200

    Update home.py

--- home.py
+++ home.py
@@ -184,7 +184,7 @@ if (btn) or (not st.session_state['recherche'].empty):
 
         # DEBUT AJOUTE LE FILMS CHOISI A LA SIDEBAR
         container = st.sidebar.container()
-        container.image(url+titres_films_proches.iloc[0]['poster_path'], width=250)
+        container.image(jacquette(url+titres_films_proches.iloc[0]['poster_path']), width=250)
         container.title(titres_films_proches.iloc[0]['Titre'])
         container.write(titres_films_proches.iloc[0]['overview'])
         # FIN DE AJOUT A LA SIDE BAR DU FILMS
